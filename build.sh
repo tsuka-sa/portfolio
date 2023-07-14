9a541ac6b173b392ecf6254816e95cb2a6e033c6
tsuka-sa <simengshui@gmail.com>
2023-07-14 21:35:09 +0900

    ok

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt 
 python manage.py collectstatic --no-input 
 python manage.py migrate 
-python manage.py superuser
+python manage.py createsuperuser
